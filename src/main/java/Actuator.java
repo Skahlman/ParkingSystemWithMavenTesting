97ab0141065e09c66b8f1933d8c7640063c2f99f
eskold <emesko20@student.hh.se>
2023-09-21 14:59:28 +0200

    ändrade till mockito test

--- src/main/java/Actuator.java
+++ src/main/java/Actuator.java
@@ -5,7 +5,7 @@ public interface Actuator {
     post-condition: returns true if the next position of the car would be okay to move to 
     properties: if position < 500 and forward = true, insideLimits(position, forward) = true
                 if the car want to move backward when it's at position 0 or moveforward at position 500,
-                
+
     */ 
     boolean insideLimits(int position, boolean forward);
     
--- src/test/java/VolvoActuatorsTest.java
+++ src/test/java/VolvoActuatorsTest.java
@@ -1,15 +1,17 @@
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.Mockito;
 import org.junit.Before;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VolvoActuatorsTest{
 
-    VolvoActuators actuator;
+    VolvoActuators actuator_mock;
 
     @Before
     public void init(){
 
-        actuator = new VolvoActuators();
+        actuator_mock = Mockito.mock(VolvoActuators.class);
 
     }
 
@@ -17,15 +19,18 @@ public class VolvoActuatorsTest{
      since it should not move forward in this case. */ 
     @Test 
     public void TestinsideLimits_MoveForwardAndEndOfStreet_returnsFalse(){
-
+        
         int position = 500;
         boolean moveforward = true;
-        
-        boolean result = actuator.insideLimits(position, moveforward);
+
+        Mockito.when(actuator_mock.insideLimits(position, moveforward)).thenReturn(false);
+
+        boolean result = actuator_mock.insideLimits(position, moveforward);
 
         assertFalse(result);
 
     }
+
     /* Testcase [2]: car is at position 0 and wants to move backward, the actuators should return false,
      since it should not move backward in this case. */ 
      @Test 
@@ -33,8 +38,10 @@ public class VolvoActuatorsTest{
 
         int position = 0;
         boolean moveforward = false;
+
+        Mockito.when(actuator_mock.insideLimits(position, moveforward)).thenReturn(false);
         
-        boolean result = actuator.insideLimits(position, moveforward);
+        boolean result = actuator_mock.insideLimits(position, moveforward);
 
         assertFalse(result);
 
@@ -48,7 +55,9 @@ public class VolvoActuatorsTest{
         int position = 5;
         boolean moveforward = true;
         
-        boolean result = actuator.insideLimits(position, moveforward);
+        Mockito.when(actuator_mock.insideLimits(position, moveforward)).thenReturn(true);
+
+        boolean result = actuator_mock.insideLimits(position, moveforward);
 
         assertTrue(result);
 
@@ -61,8 +70,10 @@ public class VolvoActuatorsTest{
 
         int position = 5;
         boolean moveforward = false;
-        
-        boolean result = actuator.insideLimits(position, moveforward);
+
+        Mockito.when(actuator_mock.insideLimits(position, moveforward)).thenReturn(true);
+
+        boolean result = actuator_mock.insideLimits(position, moveforward);
 
         assertTrue(result);
 
